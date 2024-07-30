6bcd520becafc9966bbfe0cc057773762e5e2fc4
Emanoella <emanoella.almeida@gmail.com>
2024-07-30 00:37:10 -0300

    feat: Adiciona views para registro, login e logout

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Registro
+
+def register(request):
+    return HttpResponse('Registre-se aqui!')
+
+# Login
+def login(request):
+    return HttpResponse()
+
+# Logout
+
+def logout(request):
+    return HttpResponse()
