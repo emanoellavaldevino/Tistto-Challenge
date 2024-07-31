5e5c1bb42e1b312f7e0dcb13bdd72ba5f97d715c
Emanoella <emanoella.almeida@gmail.com>
2024-07-31 15:25:23 -0300

    feat:alteração da view do app e adicionado register, login

--- tasks/views.py
+++ tasks/views.py
@@ -1,56 +1,82 @@
 from django.shortcuts import render, get_object_or_404
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Todo
 from .serializers import TodoSerializer
 
-# Decorator para especificar que a view aceita métodos GET e POST
+# View para listar e criar tarefas
 
 @api_view(["GET", "POST"])
-# Define uma view baseada na função que trata requisições para URL associada
+@permission_classes([IsAuthenticated])  # Garante que apenas usuários autenticados possam acessar esta view
 def todo_list(request):
-# Verificar se o método da requisição é GET
     if request.method == "GET":
-# Recupera todos os objetos Todo do banco de dados.
-        todos = Todo.objects.all()
-# Serializa a lista de objetos Todo
-        serializer = TodoSerializer(todos, many=True)
-# Retorna a resposta em HTTP com os dados serializados em formato JSON
+        todos = Todo.objects.filter(user=request.user)  # Filtra as tarefas do usuário autenticado
+        serializer = TodoSerializer(todos, many=True)  # Serializa a lista de tarefas
         return Response(serializer.data)
-    
-# Verifica se o método da requisição é um POST   
+
     elif request.method == "POST":
-    # Cria uma instância do serializer ' TodoSerializer ' com os dados da requisição ' request.data ' 
-        serializer = TodoSerializer(data=request.data)
-        # Valida os dados do serializer.        
+        serializer = TodoSerializer(data=request.data)  # Cria um serializer com os dados da requisição
         if serializer.is_valid():
-            # Salva o objeto Todo no banco de dados
-            serializer.save()
-            # Retorna a resposta com os dados do novo objeto Todo e o status HTTP 201 Created
-            return Response(serializer.data, status=status.HTTP_201_created)
-        #  Se os dados não forem válidos, retorna os erros com o status HTTP 400 Bad Request
+            serializer.save(user=request.user)  # Salva a tarefa associada ao usuário autenticado
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+# View para manipular uma tarefa específica
+
 @api_view(["GET", "PATCH", "PUT", "DELETE"])
+@permission_classes([IsAuthenticated])  # Garante que apenas usuários autenticados possam acessar esta view
 def todo_detail(request, pk):
-    todo = get_object_or_404(Todo, id=pk)
+    todo = get_object_or_404(Todo, id=pk)  # Recupera a tarefa ou retorna 404 se não existir
 
     if request.method == "GET":
-        serializer = TodoSerializer(todo)
+        serializer = TodoSerializer(todo)  # Serializa a tarefa
         return Response(serializer.data)
 
-
     elif request.method == "PUT":
-        serializer = TodoSerializer(todo, data=request.data)
+        serializer = TodoSerializer(todo, data=request.data)  # Atualiza a tarefa com os dados da requisição
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == "DELETE":
-        todo.delete()
+        todo.delete()  # Deleta a tarefa
         return Response(status = status.HTTP_204_NO_CONTENT)
+
+# View para registrar novos usuários
+
+@api_view(["POST"])
+def register(request):
+    if request.method == "POST":
+        username = request.data.get('username') # Obter o nome de usuário do corpo da requisição
+        password = request.data.get('password') # Obter a senha do corpo da requisição
+        if User.objects.filter(username=username).exists():  # Verificar se o usuário já existe
+            return Response({"error: Usuário não existe"}, status=status.HTTP_400_BAD_REQUEST)
+        user = User.objects.create_user(username=username, password=password) # # Criar novo usuário
+        refresh = RefreshToken.for_user(user) # Criar tokens de atualização e acesso para o usuário
+        return Response({'refresh': str(refresh), 'access': str(refresh.access_token)}, status=status.HTTP_201_CREATED)
+    
+# View para login de usuários
+
+@api_view(['POST'])
+def login(request):
+    if request.method == 'POST':
+        username = request.data.get('username') # Obter o nome de usuário do corpo da requisição
+        password = request.data.get('password')  # Obter a senha do corpo da requisição
+        user = authenticate(username=username, password=password) # Autenticar o usuário
+        if user is not None:
+            refresh = RefreshToken.for_user(user)  # Criar tokens de atualização e acesso para o usuário
+            return Response({'refresh': str(refresh), 'access': str(refresh.access_token)}, status=status.HTTP_200_OK)
+        return Response({"error": "Credenciais inválidas"}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+
+ 
 
 
 
