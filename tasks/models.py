1f2bb0d0f3d52547261e717a4afd200405af96bd
Emanoella <emanoella.almeida@gmail.com>
2024-07-31 15:33:16 -0300

    Feat: Adicionado user na models e na urls register e login

--- tasks/models.py
+++ tasks/models.py
@@ -1,25 +1,19 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 # Criando uma tabela de dados
 
 # Cada classe que herda de models.Model será traduzida em uma tabela no banco de dados 
 
 class Todo(models.Model):
-# models.CharField: Tipo de campo que armazena texto de comprimento variável.  max_length=100 o máximo de texto
-    task = models.CharField(max_length=100)
-# models.BooleanField: Tipo de campo que armazena dados booleanos e valor padrão false 
-    completed = models.BooleanField(default=False)
-# models.DateField: Tipo de campo que armazena uma data, auto_now_add=True define que a data atual será add automaticamente
-# quando um novo objeto for criado 
-    created = models.DateField(auto_now_add=True)
-# auto_now=True define que a data atual será atualizada automaticamente toda vez que o objeto for salvo
-    updated = models.DateField(auto_now=True)
+    task = models.CharField(max_length=100) #  Tipo de campo que armazena texto de comprimento variável.  
+    completed = models.BooleanField(default=False) # Tipo de campo que armazena dados booleanos e valor padrão false 
+    created = models.DateField(auto_now_add=True) # Tipo de campo que armazena uma data, auto_now_add=True define que a data atual será add automaticamente
+    updated = models.DateField(auto_now=True) # define que a data atual será atualizada automaticamente toda vez que o objeto for salvo
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Associar a tarefa ao usuário
 
-# Método __str__ para retornar o valor do campo task
-    def __str__(self):
-        return self.task
+    def __str__(self):      # Método __str__ para retornar o valor do campo task
+        return self.task 
 
     
 
--- tasks/urls.py
+++ tasks/urls.py
@@ -2,11 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('todos', views.todo_list),
-    path('todos/<int:pk>', views.todo_detail)  
-    
+    path('todos', views.todo_list), # Rota para listar e criar tarefas
+    path('todos/<int:pk>', views.todo_detail), # Rota para manipular uma tarefa específica
+    path('register', views.register), # Rota para registrar novos usuários
+    path('login', views.login),     # Rota para login de usuários
 ]
 
-# urls:
-    # http://127.0.0.1:8000/todos
-    # http://127.0.0.1:8000/todos/id
+# URLs para suas views:
+# http://127.0.0.1:8000/todos
+# http://127.0.0.1:8000/todos/<int:pk>
+# http://127.0.0.1:8000/register
+# http://127.0.0.1:8000/login
