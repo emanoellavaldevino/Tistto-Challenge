dc2b746047dab6c061342f2942829684363f07cb
Emanoella <emanoella.almeida@gmail.com>
2024-07-31 15:34:45 -0300

    feat: adicionado user no serializer

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -4,9 +4,7 @@ from .models import Todo
 # Criando uma classe serializer baseado no modelo Django para facilitar a comunicação entre API e o banco de dados
 
 class TodoSerializer(serializers.ModelSerializer):
-# A classe Meta é usada para configurar o serializer
-    class Meta:
-# model = Todo: indica que o serializer é baseado no modelo Todo
-        model = Todo
-# fields: especifica quais campos do modelo devem ser incluídos no serializer
-        fields = ['id', 'task', 'completed']
+    class Meta: # A classe Meta é usada para configurar o serializer
+        model = Todo # model = Todo: indica que o serializer é baseado no modelo Todo
+        fields = ['id', 'task', 'completed'] # fields: especifica quais campos do modelo devem ser incluídos no serializer
+        read_only_fields = ['user']
