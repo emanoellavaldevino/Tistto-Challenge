9544c8b0ce96ce90e2fbeeb6b98f02a7d6605c71
Emanoella <emanoella.almeida@gmail.com>
2024-08-01 17:53:23 -0300

    fix: alterando app.css e app.js, corrigindo códigos

--- src/App.js
+++ src/App.js
@@ -1,71 +1,62 @@
+import React, { useState } from 'react';
 import './App.css';
 
-
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+
   return ( 
-    // O componente principal da aplicação, envolvido em uma div com a classe "App".
     <div className="App">
-      <h1>Minha Tarefa</h1>
+      <h1>To Do</h1>
 
       {/* Envolvendo o formulário de entrada de tarefas em uma div com a classe "todo-wrapper". */}
-      <div className= 'todo-wrapper'>
+      <div className='todo-wrapper'>
 
-      {/* Primeira entrada de texto para o título da tarefa. */}  
-        <div className='todo-input'>       
-          <label>Título</label>
-          <input type="text" placeholder="Qual é o título da tarefa?"/> 
-        </div>
+        {/* Agrupando as entradas de título, descrição e botão "Adicionar" */}
+        <div className='todo-input'>
+          {/* Entrada de texto para o título da tarefa */}
+          <div className="todo-input-item"> 
+            <label>Título</label>          
+            <input type="text" placeholder="Qual é o título da tarefa?"/> 
+          </div>
+          
+          {/* Entrada de texto para a descrição da tarefa */}
+          <div className="todo-input-item">       
+            <label>Descrição</label>
+            <input type="text" placeholder="Qual é a descrição da tarefa?"/> 
+          </div>
 
-      {/* Segunda entrada de texto para a descrição da tarefa. */}
-        <div className='todo-input'>       
-          <label>Descrição</label>
-          <input type="text" placeholder="Qual é a descrição da tarefa?"/> 
-        </div>
-
-      {/* Botão para adicionar a tarefa, dentro de uma div com a classe "todo-input". */}
-        <div className='todo-input'>       
+          {/* Botão para adicionar a tarefa */}
           <button type='button' className='primaryBtn'>Add</button>
         </div>
-<<<<<<< HEAD
-      
 
-      {/* Área com botões de filtro de tarefas: um para mostrar todas as tarefas e outro para mostrar apenas as concluídas. */}
-             <div className='btn-area'>
-                <button>Todo</button>
-                <button>Completed</button>
-              </div>
+        {/* Área com botões de filtro de tarefas */}
+        <div className='btn-area'>
+          {/* Botão para mostrar todas as tarefas */}
+          <button 
+            className={`btn ${!isCompleteScreen ? 'activeBtn' : 'inactiveBtn'}`} 
+            onClick={() => setIsCompleteScreen(false)}>
+            To do
+          </button>
 
-      {/* Lista de tarefas, cada tarefa é envolvida em uma div com a classe "todo-list-item". */}
-              <div className='todo-list'>
+          {/* Botão para mostrar apenas as tarefas concluídas */}
+          <button 
+            className={`btn ${isCompleteScreen ? 'activeBtn' : 'inactiveBtn'}`} 
+            onClick={() => setIsCompleteScreen(true)}>
+            Completed
+          </button>
+        </div>
 
-              <div className='todo-list-item'>
-              <h3>Take 1</h3>
-              <p>Descrição</p>
-            </div>
+        {/* Lista de tarefas */}
+        <div className='todo-list'>
+          {/* Cada tarefa é envolvida em uma div com a classe "todo-list-item" */}
+          <div className='todo-list-item'>
+            <h3>Take 1</h3>
+            <p>Descrição</p>
           </div>
+        </div>
       </div>
     </div>             
-=======
-      </div>
-
-      {/* Área com botões de filtro de tarefas: um para mostrar todas as tarefas e outro para mostrar apenas as concluídas. */}
-      <div className='btn-area'>
-          <button>Todo</button>
-          <button>Completed</button>
-      </div>
-      
-      {/* Lista de tarefas, cada tarefa é envolvida em uma div com a classe "todo-list-item". */}
-      <div className='todo-list'>
-
-            <div className='todo-list-item'>
-              <h3>Task 1</h3>
-              <p>Descrição</p>
-            </div>
-
-        </div>
-      </div>             
->>>>>>> 48ef117942eb95f338f9843ddebd7a66b4938d83
   );
 }
 
-export default App; // Exporta o componente App para ser usado em outras partes da aplicação.
+export default App;
