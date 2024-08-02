729ec9d70b797c97d238212b76f14053ee61f053
Emanoella <emanoella.almeida@gmail.com>
2024-08-02 13:03:30 -0300

    Chore: adicionado o localhost do frontend

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -129,6 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
  'http://127.0.0.1:5173',
  'http://localhost:5173',
+ 'http://192.168.56.1:3000',
+ 'http://localhost:3000'
+
 ]
 
 REST_FRAMEWORK = {
