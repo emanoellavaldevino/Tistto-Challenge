b62c2fc613b7ae176275b5b74ab14045dfa61eaf
Emanoella <emanoella.almeida@gmail.com>
2024-08-01 21:28:23 -0300

    feat: alterando o app.js

--- src/App.js
+++ src/App.js
@@ -5,65 +5,98 @@ import './App.css';
 
 function App() {
   const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  const [allTodos, setTodos] = useState([]);  
+  const [newTitle, setNewTitle] = useState("");
+  const [newDescription, setNewDescription] = useState("");
 
-  return ( 
+  const handleaAddTodo = () => {
+    let newTodoItem = {
+      title: newTitle,
+      description: newDescription,
+    };
+
+    let updateTodoArr = [...allTodos];
+    updateTodoArr.push(newTodoItem);
+    setTodos(updateTodoArr);
+  };
+
+  return (
     <div className="App">
       <h1>To Do</h1>
 
       {/* Envolvendo o formulário de entrada de tarefas em uma div com a classe "todo-wrapper". */}
-      <div className='todo-wrapper'>
-
+      <div className="todo-wrapper">
         {/* Agrupando as entradas de título, descrição e botão "Adicionar" */}
-        <div className='todo-input'>
+        <div className="todo-input">
           {/* Entrada de texto para o título da tarefa */}
-          <div className="todo-input-item"> 
-            <label>Título</label>          
-            <input type="text" placeholder="Qual é o título da tarefa?"/> 
+          <div className="todo-input-item">
+            <label>Título</label>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              placeholder="Qual é o título da tarefa?"
+            />
           </div>
-          
+
           {/* Entrada de texto para a descrição da tarefa */}
-          <div className="todo-input-item">       
+          <div className="todo-input-item">
             <label>Descrição</label>
-            <input type="text" placeholder="Qual é a descrição da tarefa?"/> 
+            <input
+              type="text"
+              value={newDescription}
+              onChange={(e) => setNewDescription(e.target.value)}
+              placeholder="Qual é a descrição da tarefa?"
+            />
           </div>
 
           {/* Botão para adicionar a tarefa */}
-          <button type='button' className='primaryBtn'>Add</button>
+          <button type="button" onClick={handleaAddTodo} className="primaryBtn">
+            Add
+          </button>
         </div>
 
         {/* Área com botões de filtro de tarefas */}
-        <div className='btn-area'>
+        <div className="btn-area">
           {/* Botão para mostrar todas as tarefas */}
-          <button 
-            className={`btn ${!isCompleteScreen ? 'activeBtn' : 'inactiveBtn'}`} 
-            onClick={() => setIsCompleteScreen(false)}>
+          <button
+            className={`btn ${!isCompleteScreen ? 'activeBtn' : 'inactiveBtn'}`}
+            onClick={() => setIsCompleteScreen(false)}
+          >
             To do
           </button>
 
           {/* Botão para mostrar apenas as tarefas concluídas */}
-          <button 
-            className={`btn ${isCompleteScreen ? 'activeBtn' : 'inactiveBtn'}`} 
-            onClick={() => setIsCompleteScreen(true)}>
+          <button
+            className={`btn ${isCompleteScreen ? 'activeBtn' : 'inactiveBtn'}`}
+            onClick={() => setIsCompleteScreen(true)}
+          >
             Completed
           </button>
         </div>
 
         {/* Lista de tarefas */}
-        <div className='todo-list'>
+        <div className="todo-list">
           {/* Cada tarefa é envolvida em uma div com a classe "todo-list-item" */}
-          <div className='todo-list-item'>
-            <h3>Take 1</h3>
-            <p>Descrição</p>
-          </div>
-        </div>
-
-        <div> <AiOutlineDelete className='icon'/>
-              <BsCheckLg className='check-icon'/>
-
+          {allTodos.map((item, index) => {
+            return (
+              <div className="todo-list-item" key={index}>
+                <div>
+                  <h3>{item.title}</h3>
+                  <p>{item.description}</p>
+                </div>
+                <div className="icon-container">
+                  <AiOutlineDelete className="icon" title="delete?" />
+                  <BsCheckLg className="check-icon" title="Complete?" />
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </div>             
+    </div>
   );
 }
 
 export default App;
+
